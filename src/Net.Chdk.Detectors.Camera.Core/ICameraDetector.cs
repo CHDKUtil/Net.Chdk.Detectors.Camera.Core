bb783450f50bd6e0f94f038b1acdbf677e9b0d51
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 18:05:32 +0300

    GetCamera from CardInfo

--- src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
@@ -1,9 +1,10 @@
 ﻿using Net.Chdk.Model.Camera;
+using Net.Chdk.Model.Card;
 
 namespace Net.Chdk.Detectors.Camera
 {
     public interface ICameraDetector
     {
-        CameraInfo GetCamera(string driveLetter);
+        CameraInfo GetCamera(CardInfo cardInfo);
     }
 }
