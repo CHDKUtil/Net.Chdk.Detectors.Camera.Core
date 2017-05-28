2383fca3ba3ae6881553efbe205f6cecdaa3f1f6
Dmitry Shechtman <damnian@gmail.com>
2017-05-28 16:45:28 +0300

    Revert "Add SoftwareInfo"
    This reverts commit c3a617aa574eee85a6cfc932fa7f82c20763b1a2.

--- src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
@@ -1,11 +1,10 @@
 ﻿using Net.Chdk.Model.Camera;
 using Net.Chdk.Model.Card;
-using Net.Chdk.Model.Software;
 
 namespace Net.Chdk.Detectors.Camera
 {
     public interface ICameraDetector
     {
-        CameraInfo GetCamera(CardInfo cardInfo, SoftwareInfo softwareInfo);
+        CameraInfo GetCamera(CardInfo cardInfo);
     }
 }
