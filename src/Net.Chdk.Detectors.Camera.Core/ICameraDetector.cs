a6c80942e8166a2ae33622949e485e8e6c0efba3
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 15:03:38 +0300

    Add progress

--- src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
+++ src/Net.Chdk.Detectors.Camera.Core/ICameraDetector.cs
@@ -1,10 +1,11 @@
 ﻿using Net.Chdk.Model.Camera;
 using Net.Chdk.Model.Card;
+using System;
 
 namespace Net.Chdk.Detectors.Camera
 {
     public interface ICameraDetector
     {
-        CameraInfo GetCamera(CardInfo cardInfo);
+        CameraInfo GetCamera(CardInfo cardInfo, IProgress<double> progress);
     }
 }
